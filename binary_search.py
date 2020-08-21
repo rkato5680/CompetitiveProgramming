6198ced398f105df27c6e530ec1d22a75006d000
Ryosuke Kato <rkato5680@gmail.com>
2020-08-21 14:25:30 -0700

    add binary_search2

--- binary_search.py
+++ binary_search.py
@@ -15,3 +15,19 @@ def binary_search(func, array, left=0, right=-1):
         if right-left==1:break
 
     return left
+
+
+
+# arrayではなく整数値を検索する方式。
+# funcは条件式
+
+def binary_search2(func, n_min, n_max):
+    left,right=n_min,n_max
+    y_left, y_right = func(left), func(right)
+    while right-left>1:
+        middle = (left+right)//2
+        y_middle = func(middle)
+        if y_left==y_middle: left=middle
+        else: right=middle
+
+    return left
